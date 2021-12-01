a1ab250d02f77af7ce4ab6841fc6274af20a48ee
Dinara Kutlugildina <mnbvcxz4321@mail.ru>
2021-12-01 21:41:35 +0500

    "update"

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -18,98 +18,63 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         Session session = factory.openSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
-                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), " +
-                    "last_name VARCHAR(255), age INT)").executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
+                "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), " +
+                "last_name VARCHAR(255), age INT)").executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
         Session session = factory.openSession();
-        Transaction transaction;
-        try {
-            transaction = session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
-        try {
-            User user = new User(name, lastName, age);
-            session.save(user);
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+        User user = new User(name, lastName, age);
+        session.save(user);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
         Session session = factory.openSession();
-        Transaction transaction;
-        try {
-            transaction = session.beginTransaction();
-            User user = (User) session.get(User.class, id);
-            session.delete(user);
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+        Transaction transaction = session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+        session.delete(user);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
         Session session = factory.openSession();
-        Transaction transaction;
-        try {
-            transaction = session.beginTransaction();
-            list = session.createCriteria(User.class).list();
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+        Transaction transaction = session.beginTransaction();
+        list = session.createCriteria(User.class).list();
+        transaction.commit();
+        session.close();
         return list;
     }
 
     @Override
     public void cleanUsersTable() {
         Session session = factory.openSession();
-        try {
-            session.beginTransaction();
-            List<User> list = session.createCriteria(User.class).list();
-            for (User user : list){
-                session.delete(user);
-            }
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-
+        session.beginTransaction();
+        List<User> list = session.createCriteria(User.class).list();
+        for (User user : list){
+            session.delete(user);
         }
+        session.getTransaction().commit();
+        session.close();
     }
 }
